bfdfe3da890f362b024bfc40ec4967e2f42c20d6
Jafesu <jafesu@outlook.com>
2022-08-01 22:58:22 -0700

    EJCSDOCS-17 - Add Base FXManifest

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'EzekielJ Development Studios'
+description 'Modern Immersive Document Management system for FiveM (ESX Only)'
+version '0.0.1'
+
+shared_scripts {
+
+}
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'config.lua'
+}
+client_scripts {
+    'config.lua',
+}
+files {
+    
+}
+
+
+dependencies {
+    'es_extended',
+    'qtarget'
+}
